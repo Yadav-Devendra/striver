438267727191fd23db9545080eacd53029a5c7ba
Yadav-Devendra <yadavdevendra476@gmail.com>
2024-09-03 18:41:11 +0530

    medium

--- Pattern7.java
+++ Pattern7.java
@@ -2,8 +2,16 @@ public class Pattern7 {
 
     static void pattern1(int n){
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print("* ");
+            for (int j = 0; j < n - i - 1 ; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j < n - i - 1 ; j++) {
+                System.out.print(" ");
             }
             System.out.println();
         }
--- Pattern8.java
+++ Pattern8.java
@@ -1,3 +1,24 @@
 public class Pattern8 {
-    
+
+    static void pattern1(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < n + 2 - (2 * i); j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        pattern1(n);
+    }
 }
